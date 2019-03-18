3cfb3bffb68bcce063e1740cef9d5b9e2da21c83
sujiahong <1511877962@qq.com>
2019-03-18 09:27:36 +0800

    lua

---
+++ lapi.h
@@ -0,0 +1,14 @@
+#ifndef lapi_h
+#define lapi_h
+
+#include "llimits.h"
+#include "lstate.h"
+
+#define api_incr_top(L){ L->top++; api_check(L, L->top <= L->ci->top, "statck overflow");}
+#define adjustresults(L, nres){ if ((nres) <= LUA_MULTRET && L->ci->top < L->top) L->ci->top = L->top;}
+#define api_checknelems(L,n) api_check(L, (n)<(L->top-L->ci->func), "not enough elements in the stack")
+
+#define hastocloseCfunc(n) ((n) < LUA_MULTRET)
+#define codeNresults(n) (-(n) - 3)
+
+#endif
